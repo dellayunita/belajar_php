32a2d4db35ec0815947fb9716e548f8d696bf263
Della Yunita <dellayunita040702@gmail.com>
2022-11-03 21:14:51 +0700

    belajar php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo "Hello World!<br>";
+
+$nama="Della Yunita";
+$umur=20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik="Atthar";
+$umurAdik=1;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur= $umur-$umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+ ?>
